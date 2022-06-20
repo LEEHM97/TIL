e22b67974ecc9f12133ab00a24714d5a9a62ff87
LEEHM97 <gmlkd2006@gmail.com>
2022-06-21 00:15:19 +0900

    solve 4673

--- coding-test/백준/백준 단계별/5.함수/4673.py
+++ coding-test/백준/백준 단계별/5.함수/4673.py
@@ -1,3 +1,19 @@
 def dn(n):
-    pass
+    result = n
+    a = n
+    while n>9:
+        a, b = divmod(n, 10)
+        result += b
+        n = a
+    result += a
+    return result
 
+self_num = []
+num = list(i for i in range(10001))
+
+for i in range(0, 10001):
+    self_num.append(dn(i))
+
+for i in num:
+    if i not in self_num:
+        print(i)    
