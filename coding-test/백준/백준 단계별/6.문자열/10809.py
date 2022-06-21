cf71ea6e8855e192d3b04a94c7d225beda1a83a3
LEEHM97 <gmlkd2006@gmail.com>
2022-06-21 19:14:57 +0900

    solve 10809

--- coding-test/백준/백준 단계별/6.문자열/10809.py
+++ coding-test/백준/백준 단계별/6.문자열/10809.py
@@ -1,8 +1,16 @@
 s = list(map(str, input()))
-alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','n','m','o','p','q','r','s','t','u','v','w','x','y','z']
+alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 for i in alpha:
     if i in s:
         print(f'{s.index(i)}', end=' ')
     else:
         print('-1', end=' ')
+        
+        
+        
+# word = input().lower()
+# alphabet = list(range(97, 123))
+
+# for x in alphabet:
+#     print(word.find(chr(x)), end=' ')
