ea2bb4445168b28056ebda9a4eff19d0298d4ee2
LEEHM97 <gmlkd2006@gmail.com>
2022-06-22 00:44:36 +0900

    solve with solution

--- coding-test/백준/백준 단계별/6.문자열/2941.py
+++ coding-test/백준/백준 단계별/6.문자열/2941.py
@@ -11,3 +11,11 @@
 #     else:
 #         cnt -= word.count(i)
 # print(cnt)
+
+
+croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+word = input()
+
+for i in croatia :
+    word = word.replace(i, '*')
+print(len(word))
