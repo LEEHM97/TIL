14670cd6895e3503a4daa93eec7de0b3487bb908
LEEHM97 <gmlkd2006@gmail.com>
2022-06-22 00:43:01 +0900

    don't solve

---
+++ coding-test/백준/백준 단계별/6.문자열/2941.py
@@ -0,0 +1,13 @@
+## wrong answer, why..?
+# croatia = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+
+# word = input()
+# cnt = len(word)
+
+# for i in croatia:
+#     if i == 'dz=':
+#         cnt -= word.count(i) * 2
+#         word = word.replace(i, "")
+#     else:
+#         cnt -= word.count(i)
+# print(cnt)
