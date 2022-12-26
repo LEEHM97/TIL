32b84aa6306efdf4464074c3e0fe0b13cf528e62
LEEHM97 <gmlkd2006@gmail.com>
2022-12-26 22:55:16 +0900

    solve 1764

---
+++ coding-test/백준/백준 단계별/13. 집합과 맵/1764.py
@@ -0,0 +1,37 @@
+n, m = map(int, input().split())
+
+n_list = []
+m_list = []
+result = []
+cnt = 0
+
+for i in range(n):
+    person = input()
+    n_list.append(person)
+    
+for _ in range(m):
+    person = input()
+    m_list.append(person)
+    
+n_list = set(n_list)
+m_list = set(m_list)
+
+min_lst = min(n, m)
+
+if min_lst == n:
+    for i in n_list:
+        if i in m_list:
+            cnt += 1
+            result.append(i)
+
+else:
+    for i in m_list:
+        if i in n_list:
+            cnt += 1
+            result.append(i)
+ 
+result.sort()            
+
+print(cnt)
+for i in result:
+    print(i)
