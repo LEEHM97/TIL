78a985f132230912c45c90aa92cb22037abe8eb1
LEEHM97 <gmlkd2006@gmail.com>
2023-01-21 23:21:08 +0900

    sikve 4949

---
+++ coding-test/백준/백준 단계별/16.스택/4949.py
@@ -0,0 +1,34 @@
+import sys
+
+
+while True:
+    words = input()
+    stack = []
+    if words == '.':
+        break
+    
+    for i in range(len(words)):
+        if words[i] == '[':
+            stack.append(words[i])
+        elif words[i] == '(':
+            stack.append(words[i])
+        elif words[i] == ')':
+            if len(stack) == 0 or stack[-1] != '(':
+                print(len(stack))
+                print('no')
+                break
+            else:
+                stack.pop()
+        elif words[i] == ']':
+            if len(stack) == 0 or stack[-1] != '[':
+                print('no')
+                break
+            else:
+                stack.pop()
+        elif i==len(words)-1 and words[i] == '.':
+            if len(stack) > 0:
+                print('no')
+            else:
+                print('yes')
+        else:
+            continue
