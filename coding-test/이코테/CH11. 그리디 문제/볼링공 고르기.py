249f6d5cd176df65dfd1d6998c72dba3407ee168
LEEHM97 <gmlkd2006@gmail.com>
2023-02-22 23:32:13 +0900

    solve 볼링공 고르기

--- coding-test/이코테/CH11. 그리디 문제/볼링공 고르기.py
+++ coding-test/이코테/CH11. 그리디 문제/볼링공 고르기.py
@@ -1,3 +1,14 @@
 import sys
 input = sys.stdin.readline
 
+n, m = map(int, input().split())
+
+ball = list(map(int, input().split()))
+result = 0
+
+for i in range(len(ball)-1):
+    for j in range(i, len(ball)):
+        if ball[i] != ball[j]:
+            result += 1
+
+print(result)
