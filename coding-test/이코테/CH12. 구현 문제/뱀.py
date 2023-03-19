bb1b6c36a62e92178e167ddc547fa2bba9ac564a
LEEHM97 <gmlkd2006@gmail.com>
2023-03-19 23:19:32 +0900

    solve with solution

--- coding-test/이코테/CH12. 구현 문제/뱀.py
+++ coding-test/이코테/CH12. 구현 문제/뱀.py
@@ -25,3 +25,36 @@ def turn(direction, c):
     else:
         direction = (direction + 1) % 4
     return direction
+
+def simulate():
+    x, y = 1, 1
+    data[x][y] = 2 # 뱀이 존재하는 위치
+    direction = 0
+    time = 0
+    index = 0
+    q = [(x, y)]
+    
+    while True:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        if 1 <= nx and nx <= n and 1 <= ny and ny <= n and data[nx][ny] != 2:
+            if data[nx][ny] == 0:
+                data[nx][ny] = 2
+                q.append((nx, ny))
+                px, py = q.pop(0)
+                data[px][py] = 0
+            
+            if data[nx][ny] == 1:
+                data[nx][ny] = 2
+                q.append((nx, ny))
+        else:
+            time += 1
+            break
+        x, y = nx, ny
+        time += 1
+        if index < 1 and time == info[index][0]:
+            direction = turn(direction, info[index][1])
+            index += 1
+        return time
+
+print(simulate)
