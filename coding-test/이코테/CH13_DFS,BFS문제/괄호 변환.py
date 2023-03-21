bbfa9e20e1981df964821793fd6e30e242047567
LEEHM97 <gmlkd2006@gmail.com>
2023-03-22 01:16:47 +0900

    feat: check proper function

--- coding-test/이코테/CH13_DFS,BFS문제/괄호 변환.py
+++ coding-test/이코테/CH13_DFS,BFS문제/괄호 변환.py
@@ -8,3 +8,15 @@ def balanced_index(p):
             count -= 1
         if count == 0:
             return 1
+    
+# 올바른 괄호 문자열 판단    
+def check_proper(p):
+    count = 0
+    for i in p:
+        if i == '(':
+            count += 1
+        else:
+            if count == 0:
+                return False
+            count -= 1
+    return True
