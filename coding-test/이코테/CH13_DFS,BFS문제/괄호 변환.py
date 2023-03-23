b473cf717cbce8699a950365080ccbf5deb6ff81
LEEHM97 <gmlkd2006@gmail.com>
2023-03-23 23:35:39 +0900

    solve 괄호 변환

--- coding-test/이코테/CH13_DFS,BFS문제/괄호 변환.py
+++ coding-test/이코테/CH13_DFS,BFS문제/괄호 변환.py
@@ -20,3 +20,27 @@ def check_proper(p):
                 return False
             count -= 1
     return True
+
+def solution(p):
+    answer = ''
+    if p == '':
+        return answer
+    index = balanced_index(p)
+    u = p[:index + 1]
+    v = p[index + 1 :]
+    
+    if check_proper(u):
+        answer = u + solution(v)
+        
+    else:
+        answer = '('
+        answer += solution(v)
+        answer += ')'
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+        answer += "".join(u)
+    return answer
